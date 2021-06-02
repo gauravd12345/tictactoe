1a9d5c47a819c21c0257e59861c73697d6b7762e
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2021-06-02 14:09:04 -0700

    implemented AI

--- oneplayer.js
+++ oneplayer.js
@@ -1,5 +1,5 @@
-var board = [['X', 'X', 'O'],
-             [' ', 'O', ' '],
+var board = [[' ', ' ', ' '],
+             [' ', ' ', ' '],
              [' ', ' ', ' ']];
 
 var idList = [["box1", "box2", "box3"], 
@@ -11,6 +11,8 @@ var count = 0;
 var gameOver = false;
 var min = "O";
 var max = "X";
+var human = max;
+var ai = min;
 
 function printBoard(){
     for(var i = 0; i < board.length; i++){
@@ -26,7 +28,7 @@ function printBoard(){
 function checkStatus(board){
     for(var i = 0; i < board.length; i++){
         for(var j = 0; j < board[i].length; j++){
-            if(board[i][0] == board[i][1]  && board[i][1] == board[i][2] && board[i][0] != " "){
+            if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != " "){
                 return board[i][0];
             }    
                
@@ -49,20 +51,16 @@ function checkStatus(board){
         return board[2][0];
 
     }
-    if(getAvailableMoves(board).length == 0){
-        return " ";
 
-    } 
+    
     return null;
-
-
 
 }
 
 
 function displayStatus(){
     var winner = checkStatus(board);
-    if(checkStatus(board) != null || count == 9){
+    if(getAvailableMoves(board).length == 0 || winner != null){
         gameOver = true;
 
     }
@@ -89,92 +87,32 @@ function displayStatus(){
 
 }
 
-function getPlayer(){
-    if(count % 2 == 0){
+
+function getAvailableMoves(board){
+    var moveList = [];
+    for(var i = 0; i < board.length; i++){
+        for(var j = 0; j < board[i].length; j++){
+            if(board[i][j] == " "){
+                var result = [i, j];
+                moveList.push(result);
+            }
+
+        }
+    }
+    return moveList;
+
+}
+
+
+function getPlayer(board){
+    console.log(board);
+    if(getAvailableMoves(board).length % 2 == 1){
         return "X";
 
     }
     return "O";
 
 }
-
-function placeMove(move){
-    if(!gameOver){
-        if(move == 1 && board[0][0] == " "){setPiece(board, max, 0, 0);}
-        else if(move == 2 && board[0][1] == " "){setPiece(board, max, 0, 1);}
-        else if(move == 3 && board[0][2] == " "){setPiece(board, max, 0, 2);}
-        else if(move == 4 && board[1][0] == " "){setPiece(board, max, 1, 0);}
-        else if(move == 5 && board[1][1] == " "){setPiece(board, max, 1, 1);}
-        else if(move == 6 && board[1][2] == " "){setPiece(board, max, 1, 2);}
-        else if(move == 7 && board[2][0] == " "){setPiece(board, max, 2, 0);}
-        else if(move == 8 && board[2][1] == " "){setPiece(board, max, 2, 1);}
-        else if(move == 9 && board[2][2] == " "){setPiece(board, max, 2, 2);}
-        else{alert("Enter a valid move");return;}
-        console.log(getBestMove(board));
-        printBoard();
-        displayStatus();
-        
-
-        
-    }
-    
-
-}
-
-
-function getBestMove(board){
-    var bestMove;
-    var bestScore = Infinity;
-    var availableMoves = getAvailableMoves(board);
-    for(var i = 0; i < availableMoves.length; i++){
-        var score = minimax(board, min, false);
-        if(score < bestScore){
-            bestScore = score;
-            bestMove = i;
-
-        }
-    }
-    console.log(availableMoves, availableMoves[bestMove]);
-
-}
-
-function minimax(board, player, isMaximizing){
-    if(isTerminal(board) != null){
-        return isTerminal(board);
-    }
-
-    if(isMaximizing){
-        var bestScore = -Infinity;
-        var availableMoves = getAvailableMoves(board);
-        for(var i = 0; i < availableMoves.length; i++){
-            setPiece(board, player, availableMoves[i][0], availableMoves[i][1]);
-            bestScore = Math.max(bestScore, minimax(board, min));
-            setPiece(board, " ", availableMoves[i][0], availableMoves[i][1]);
-
-        }
-        return bestScore;
-
-    }
-    else{
-        var bestScore = Infinity;
-        var availableMoves = getAvailableMoves(board);
-        for(var i = 0; i < availableMoves.length; i++){
-            setPiece(board, player, availableMoves[i][0], availableMoves[i][1]);
-            bestScore = Math.min(bestScore, minimax(board, max));
-            setPiece(board, " ", availableMoves[i][0], availableMoves[i][1]);
-
-        }
-        return bestScore;
-
-    }
-    
-    
-}
-
-
-
-
-
 function handleInput(move){
     if(event.key == "Enter"){
         if(1 <= move.value && move.value <= 9){
@@ -191,45 +129,149 @@ function handleInput(move){
 
 }
 
-function setPiece(board, piece, i, j){
-    board[i][j] = piece;
+function setPiece(board, action){
+    var newBoard = [[' ', ' ', ' '],
+                    [' ', ' ', ' '],
+                    [' ', ' ', ' ']];
+
+    for(var x = 0; x < board.length; x++){
+        for(var y = 0; y < board[x].length; y++){ 
+            newBoard[x][y] = board[x][y];
+
+        }
+
+    }
+
+    newBoard[action[0]][action[1]] = getPlayer(board);
+    return newBoard; 
 
 }
 
+
 function isTerminal(board){
-    if(checkStatus(board) == "X"){
+    if(checkStatus(board) != null){
+        return true;
+
+    }
+    else if(checkStatus(board) == null && getAvailableMoves(board).length == 0){
+        return true;
+
+    }
+    return false;
+
+}
+
+function utility(board){
+    var winner = checkStatus(board);
+    if(winner == "X"){
         return 1;
 
     }
-    else if(checkStatus(board) == "O"){
+    else if(winner == "O"){
         return -1;
 
     }
-    else if(checkStatus(board) == " "){
-        return 0;
 
-    }
-    return null;
+    return 0;
 
 }
 
 
-function getAvailableMoves(board){
-    var moveList = [];
-    for(var i = 0; i < board.length; i++){
-        for(var j = 0; j < board[i].length; j++){
-            if(board[i][j] == " "){
-                moveList.push([i, j]);
+function placeMove(move){
+    if(!gameOver){
+        if(move == 1 && board[0][0] == " "){board[0][0] = human;}
+        else if(move == 2 && board[0][1] == " "){board[0][1] = human;}
+        else if(move == 3 && board[0][2] == " "){board[0][2] = human;}
+        else if(move == 4 && board[1][0] == " "){board[1][0] = human;}
+        else if(move == 5 && board[1][1] == " "){board[1][1] = human;}
+        else if(move == 6 && board[1][2] == " "){board[1][2] = human;}
+        else if(move == 7 && board[2][0] == " "){board[2][0] = human;}
+        else if(move == 8 && board[2][1] == " "){board[2][1] = human;}
+        else if(move == 9 && board[2][2] == " "){board[2][2] = human;}
+        else{alert("Enter a valid move");return;}
+        var result = getBestMove(board);
+        //console.log(result);
+        board[result[0]][result[1]] = getPlayer(board);
+        printBoard();
+        displayStatus();
+        
+
+        
+    }
+    
+
+}
+
+function minValue(board){
+    if(isTerminal(board)){
+        return utility(board);
+    }
+
+    var bestScore = Infinity;
+    for(var i = 0; i < getAvailableMoves(board).length; i++){
+        bestScore = Math.min(bestScore, maxValue(setPiece(board, getAvailableMoves(board)[i])));
+
+    }
+
+    return bestScore;  
+
+}
+
+function maxValue(board){
+    if(isTerminal(board)){
+        return utility(board);
+    }
+
+    var bestScore = -Infinity;
+    for(var i = 0; i < getAvailableMoves(board).length; i++){
+        bestScore = Math.max(bestScore, minValue(setPiece(board, getAvailableMoves(board)[i])));
+
+    }
+
+    return bestScore;  
+
+}
+
+
+function getBestMove(board){
+    if(getPlayer(board) == "X"){
+        var bestScore = -Infinity;
+        //console.log("Ran through getPlayer", getAvailableMoves(board));
+        for(var i = 0; i < getAvailableMoves(board).length; i++){
+            //console.log(score, bestScore, bestMove, getAvailableMoves(board)[i]);
+            var score = minValue(setPiece(board, getAvailableMoves(board)[i]));
+            if(score > bestScore){
+                bestScore = score;
+                var bestMove = getAvailableMoves(board)[i];
             }
-
         }
+
+
+    }
+    else if(getPlayer(board) == "O"){
+        var bestScore = Infinity;
+        //console.log("Ran through getPlayer");
+        for(var i = 0; i < getAvailableMoves(board).length; i++){
+            var score = maxValue(setPiece(board, getAvailableMoves(board)[i]));
+            //console.log(score, bestScore, bestMove, getAvailableMoves(board)[i]);
+            if(score < bestScore){
+                bestScore = score;
+                var bestMove = getAvailableMoves(board)[i];
+            }
+        }
+
+
     }
 
-    return moveList;
+    return bestMove;
 
 }
 
+if(human == min){
+    var result = getBestMove(board);
+    board[result[0]][result[1]] = getPlayer(board);
 
+}
 printBoard();
 displayStatus();
 
